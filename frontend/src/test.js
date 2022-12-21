0203fbe86b0744302414a27ac3ed7b9152d8f720
Sampsa Heinonen <sampsavh@gmail.com>
2022-12-21 16:45:56 +0200

    removed ts

--- frontend/src/test.js
+++
@@ -1,4 +0,0 @@
-fetch('http://assignments.reaktor.com/birdnest/drones')
-        .then(response => response.text())
-        .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
-        .then(data => console.log(data));
